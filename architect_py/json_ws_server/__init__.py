485a3ed5d78f2ff1c2256c5046d8af0a98e6fe9a
Andrew Cho <a.cho@architect.co>
2024-09-23 14:03:20 -0400

    mid progress

--- architect_py/json_ws_server/__init__.py
+++ architect_py/json_ws_server/__init__.py
@@ -4,41 +4,40 @@ import json
 import logging
 
 
-import asyncio
-
-from websockets.asyncio.server import serve
+from websockets.asyncio.server import serve, ServerConnection
 
 from typing import Any, Dict
 
 
 class JsonWsServer:
     def __init__(self) -> None:
-        self.subscriptions = {}
+        pass
 
     async def serve(self, host: str, port: int) -> None:
         logging.critical(f"Starting server on {host}:{port}")
         await serve(self.accept_connection, host, port)
 
-    async def accept_connection(self, websocket):
+    async def accept_connection(self, websocket: ServerConnection):
         try:
             async for message in websocket:
                 data = json.loads(message)
-                response = await self.handle_message(data)
+                response = await self.handle_message(data, websocket)
                 await websocket.send(json.dumps(response))
         except ConnectionClosed as e:
             print(f"Connection closed: {e}")
 
-    async def handle_message(self, message: Dict[str, Any]) -> Dict[str, Any]:
+    async def handle_message(
+        self, message: Dict[str, Any], websocket: ServerConnection
+    ) -> Dict[str, Any]:
         message_type = message.get("type")
         if message_type == "query":
             return await self.handle_query(message)
         elif message_type == "subscribe":
-            return await self.handle_subscribe(message)
+            return await self.handle_subscribe(message, websocket)
         else:
             return {"status": "error", "error": "Unknown message type"}
 
     async def handle_query(self, message: Dict[str, Any]) -> Dict[str, Any]:
-        raise NotImplementedError
         method = message.get("method")
         params = message.get("params")
         if method == "symbology/snapshot":
@@ -48,25 +47,54 @@ class JsonWsServer:
                 "result": self.get_symbology_snapshot(),
             }
         elif method == "marketdata/book/l2/snapshot":
-            return {
-                "id": message["id"],
-                "type": "response",
-                "result": self.get_l2_book_snapshot(params),
-            }
+            if params is not None:
+                return {
+                    "id": message["id"],
+                    "type": "response",
+                    "result": self.get_l2_book_snapshot(params),
+                }
+            else:
+                return {
+                    "id": message["id"],
+                    "type": "response",
+                    "result": "error: params was None",
+                }
         elif method == "marketdata/book/l3/snapshot":
-            return {
-                "id": message["id"],
-                "type": "response",
-                "result": self.get_l3_book_snapshot(params),
-            }
+            if params is not None:
+                return {
+                    "id": message["id"],
+                    "type": "response",
+                    "result": self.get_l3_book_snapshot(params),
+                }
+            else:
+                return {
+                    "id": message["id"],
+                    "type": "response",
+                    "result": "error: params was None",
+                }
         else:
             return {"id": message["id"], "type": "response", "error": "Unknown method"}
 
-    async def handle_subscribe(self, message: Dict[str, Any]) -> Dict[str, Any]:
-        raise NotImplementedError
+    async def handle_subscribe(
+        self, message: Dict[str, Any], websocket: ServerConnection
+    ) -> Dict[str, Any]:
         topic = message.get("topic")
-        self.subscriptions[topic] = message["id"]
+
+        if topic is None:
+            return {"id": message["id"], "type": "response", "error": "No topic provided"}
+        
+        
+
+        asyncio.create_task(self._subscribe(topic, websocket))
         return {"id": message["id"], "type": "response", "status": "subscribed"}
+
+    async def _subscribe(self, topic: str, websocket: ServerConnection):
+        # acho CR: This is a potentially inoptimal implementation
+        await asyncio.sleep(5)
+
+
+        await websocket.send(json.dumps(response))
+
 
     def get_symbology_snapshot(self) -> Dict[str, Any]:
         raise NotImplementedError
