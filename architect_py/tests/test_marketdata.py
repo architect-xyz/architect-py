366a22f70e43b258efbc430f11aacc1f223688c8
Andrew Lee <126016409+andrew-lee-architect@users.noreply.github.com>
2024-12-01 18:05:46 -0600

    _

--- architect_py/tests/test_marketdata.py
+++ architect_py/tests/test_marketdata.py
@@ -2,12 +2,12 @@ import pytest
 from architect_py.async_client import AsyncClient
 
 
-# CR alee: this test only works if the market is open..
+# CR alee: this test only works if the market is open and trading actively..
 @pytest.mark.asyncio
 @pytest.mark.timeout(2)
-async def test_subscribe_trades(async_client: AsyncClient):
-    markets = await async_client.search_markets(sort_by_volume_desc=True, max_results=1)
-    market = markets[0]
+async def test_subscribe_cme_trades(async_client: AsyncClient):
+    markets = await async_client.get_cme_futures_series("ES")
+    _, market = markets[0]
     trades = async_client.subscribe_trades(market.name)
     for _ in range(5):
         trade = await anext(trades)
