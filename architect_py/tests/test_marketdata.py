2457bc29a468fe34c315b344cab333d90acef5c5
Andrew Lee <126016409+andrew-lee-architect@users.noreply.github.com>
2024-12-04 10:10:11 -0600

    disable flaky test

--- architect_py/tests/test_marketdata.py
+++ architect_py/tests/test_marketdata.py
@@ -3,12 +3,12 @@ from architect_py.async_client import AsyncClient
 
 
 # CR alee: this test only works if the market is open and trading actively..
-@pytest.mark.asyncio
-@pytest.mark.timeout(2)
-async def test_subscribe_cme_trades(async_client: AsyncClient):
-    markets = await async_client.get_cme_futures_series("ES")
-    _, market = markets[0]
-    trades = async_client.subscribe_trades(market.name)
-    for _ in range(5):
-        trade = await anext(trades)
-        assert trade is not None, "trade from stream was None"
+# @pytest.mark.asyncio
+# @pytest.mark.timeout(2)
+# async def test_subscribe_cme_trades(async_client: AsyncClient):
+#     markets = await async_client.get_cme_futures_series("ES")
+#     _, market = markets[0]
+#     trades = async_client.subscribe_trades(market.name)
+#     for _ in range(5):
+#         trade = await anext(trades)
+#         assert trade is not None, "trade from stream was None"
