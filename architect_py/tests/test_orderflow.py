6647ba3af081f3b57942632500f9cbfac2c37b3b
sage-farrenholz-architect <sage@architect.xyz>
2024-12-13 17:04:20 -0600

    basic market order testing

--- architect_py/tests/test_orderflow.py
+++ architect_py/tests/test_orderflow.py
@@ -1,3 +1,5 @@
+import logging
+
 import pytest
 
 from architect_py.async_client import AsyncClient, OrderDir
@@ -13,29 +15,31 @@ async def test_market_pro_order(async_client: AsyncClient):
     pass
 
 
+@pytest.mark.live_orderflow
 @pytest.mark.asyncio
 @pytest.mark.timeout(3)
 @pytest.mark.parametrize(
     "market_id",
     [
-        "BTC Crypto/USDC Crypto*BINANCE/DIRECT",
-        # "ETH Crypto/USDC Crypto*BINANCE/DIRECT",
+        "BTC Crypto/USDT Crypto*BINANCE/DIRECT",
+        "ETH Crypto/USDT Crypto*BINANCE/DIRECT",
     ],
 )
-async def test_basic_order(async_client: AsyncClient, market_id: str):
-    # Assert market is available
+async def test_live_market_order(async_client: AsyncClient, market_id: str):
+    # Get snapshot
     market = await async_client.get_market(market_id)
     assert market is not None, f"Market does not exist for {market_id}"
 
-    # Hopefully we get some btc at this price!
-    limit_price = 10.00
+    snapshot = await async_client.get_market_snapshot(market_id)
+    assert snapshot is not None, f"Snapshot does not exist for {market_id}"
 
     # Make a very cheap
     order = await async_client.send_limit_order(
-        market=market.id,
+        market=market_id,
         odir=OrderDir.BUY,
-        quantity=market.min_order_quantity,
-        limit_price=limit_price,
+        quantity=market.min_order_quantity
+        * 10,  # not sure what's going here with min qty not being accurate
+        limit_price=snapshot.ask_price * 1.15,
     )
 
     assert order is not None
