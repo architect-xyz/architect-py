519caaaf1b0ba545a5e4f3c7a09b8ef47cef438d
Andrew Cho <a.cho@architect.co>
2025-02-26 05:02:59 -0500

    test endpoint

--- architect_py/tests/test_grpc_client.py
+++ architect_py/tests/test_grpc_client.py
@@ -3,6 +3,7 @@ import os
 
 from architect_py.grpc_client import GRPCClient
 from architect_py.graphql_client.client import GraphQLClient
+from architect_py.scalars import TradableProduct
 
 
 async def main():
@@ -20,8 +21,13 @@ async def main():
         host=host, port=port, api_key=api_key, api_secret=api_secret
     )
 
+    endpoint = "app.architect.co"
+    endpoint = "binance.marketdata.architect.co"
+
     grpc_client = GRPCClient(graphql_client)
-    await grpc_client.grpc_channel("app.architect.co")
+
+    async for update in grpc_client.subscribe_l1_book_snapshots(endpoint):
+        print(update)
 
 
 if __name__ == "__main__":
