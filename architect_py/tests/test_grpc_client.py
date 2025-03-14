3aa932440d82cd6a89fc8d93c2df0de791ca7396
Andrew Cho <a.cho@architect.co>
2025-03-14 14:24:22 -0400

    update

--- architect_py/tests/test_grpc_client.py
+++ architect_py/tests/test_grpc_client.py
@@ -16,6 +16,9 @@ async def test_grpc(async_client: AsyncClient, front_ES_future: str):
     snapshot = await async_client.grpc_client.request_l2_book_snapshot("CME", tp)
     assert snapshot is not None
 
+    snapshot = await async_client.grpc_client.request_l1_book_snapshot(tp)
+    assert snapshot is not None
+
     i = 0
     async for update in await async_client.grpc_client.subscribe_l1_books_stream(
         [front_ES_future]
