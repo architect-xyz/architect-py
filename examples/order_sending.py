aa6947ecb7bc53116d547d28f0089cb0ca96f840
Andrew Cho <a.cho@architect.co>
2025-03-24 11:36:48 -0400

    client protocol

--- examples/order_sending.py
+++ examples/order_sending.py
@@ -4,7 +4,8 @@ from decimal import Decimal
 import logging
 
 from architect_py.async_client import AsyncClient
-from architect_py.graphql_client.enums import OrderType, TimeInForce
+from architect_py.grpc_client.Oms.PlaceOrderRequest import PlaceOrderRequestType
+from architect_py.grpc_client.definitions import TimeInForceEnum
 from architect_py.scalars import OrderDir, TradableProduct
 
 LOGGER = logging.getLogger(__name__)
@@ -54,7 +55,7 @@ async def test_send_order():
         limit_price=snapshot.bid_price
         - (snapshot.ask_price - snapshot.bid_price) * Decimal(10),
         account=ACCOUNT,
-        time_in_force=TimeInForce.IOC,
+        time_in_force=TimeInForceEnum.IOC,
     )
     logging.critical(f"ORDER TEST: {order}")
 
@@ -79,7 +80,7 @@ async def test_send_market_pro_order():
         odir=OrderDir.BUY,
         quantity=Decimal(1),
         account=ACCOUNT,
-        time_in_force=TimeInForce.IOC,
+        time_in_force=TimeInForceEnum.IOC,
     )
 
 
