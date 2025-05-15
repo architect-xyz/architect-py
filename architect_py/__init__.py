11043a36d791a36df17407d5ec6e685ff29f5cac
Andrew Lee <126016409+andrew-lee-architect@users.noreply.github.com>
2025-05-15 18:29:32 -0500

    _

--- architect_py/__init__.py
+++ architect_py/__init__.py
@@ -3,13 +3,14 @@ __version__ = "5.0.0"
 from .async_client import AsyncClient
 from .client import Client
 from .common_types import OrderDir, TradableProduct
-from .grpc import CandleWidth, L2BookDiff, L2BookSnapshot, TimeInForceEnum
+from .grpc import CandleWidth, L2BookDiff, L2BookSnapshot, OrderStatus, TimeInForceEnum
 
 __all__ = [
     "AsyncClient",
     "Client",
     "CandleWidth",
     "OrderDir",
+    "OrderStatus",
     "TradableProduct",
     "TimeInForceEnum",
     "L2BookSnapshot",
